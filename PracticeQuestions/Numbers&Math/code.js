a617d7e2e10fcc21ed8183c619271ba4da1d4b87
birbalkr <birbalkr1435@gmail.com>
2026-05-23 21:40:04 +0530

    solve Numbers & Math question

---
+++ PracticeQuestions/Numbers&Math/code.js
@@ -0,0 +1,23 @@
+// Round 4.7 using Math.round().
+console.log(Math.round(4.7));
+
+// Find the square root of 81
+console.log(Math.sqrt(81));
+
+// Find the maximum number from 10, 20, 5, 99.
+console.log(Math.max(10, 20, 5, 99));
+
+// Generate a random number between 1 and 10.
+console.log(Math.floor(Math.random()*10));
+
+// Convert "99.99" into an integer.
+let numString = 99.99;
+console.log(parseInt(numString));
+
+// Check whether 25 is an integer or not.
+x=25;
+console.log(x===parseInt(x)? "25 is an integer." : "25 is not an integer.");
+
+// Use toFixed(2) on 3.141592.
+let pi = 3.141592;
+console.log(pi.toFixed(2));
