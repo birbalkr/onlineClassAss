c55cac42dd7156957aa3a9dbf6a14f2a5a3771fb
birbalkr <birbalkr1435@gmail.com>
2026-05-23 21:31:20 +0530

    solve string question

--- PracticeQuestions/Strings/String2.js
+++ PracticeQuestions/Strings/String2.js
@@ -1,3 +1,44 @@
 // Convert a string into uppercase.
 let myString = "Hello, World!";
 console.log(myString.toUpperCase());
+
+// Convert a string into uppercase.
+console.log(myString.toLowerCase());
+
+// Check if a string includes the word "JavaScript".
+let anotherString = "I love JavaScript!";
+console.log(anotherString.includes("JavaScript"));
+
+// Extract the word "World" from "Hello World".
+
+// let extractedWord = ;
+console.log(myString.substring(7, 12));
+
+// Replace "apple" with "mango" in a sentence.
+
+let sentence= "     I have an apple.";
+console.log(sentence.replace("apple","mango"));
+
+// Split "HTML,CSS,JS" into an array.
+
+let languages = "HTML,CSS,JS";
+console.log(languages.split(""));
+
+// Remove extra spaces from a string.
+
+console.log(sentence.trim());
+
+// Repeat the word "Hi" 5 times.
+let word = "Hi ";
+console.log(word.repeat(5));
+
+// Print the first character of a string.
+
+console.log(word.charAt(0));
+
+// Use template literals to print:"My name is Aman and I am 20 years old"
+
+name = "Aman";
+age = 20;
+console.log(`My name is ${name} and I am ${age} years old.`);
+
