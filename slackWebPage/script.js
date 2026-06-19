55423cd68b71de09c9ff57b2bb8e1e6008ff444d
birbalkr <birbalkr1435@gmail.com>
2026-06-19 14:40:04 +0530

    features dropdown

---
+++ slackWebPage/script.js
@@ -0,0 +1,8 @@
+let dropdown = document.querySelector(".dropdown");
+let dropdownItems = document.querySelector(".dropdownItem");
+
+dropdown.addEventListener("click", () => {
+    dropdownItems.classList.toggle("hidden");
+    
+    
+})
