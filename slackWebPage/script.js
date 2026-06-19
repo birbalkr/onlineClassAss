126cf5c1d19e89cd50ded72a0270162e6f8c015e
birbalkr <birbalkr1435@gmail.com>
2026-06-19 20:22:49 +0530

    Solutions dropdown

--- slackWebPage/script.js
+++ slackWebPage/script.js
@@ -3,6 +3,11 @@ let dropdownItems = document.querySelector(".dropdownItem");
 
 dropdown.addEventListener("click", () => {
     dropdownItems.classList.toggle("hidden");
-    
-    
+})
+
+let dropdown1=document.querySelector(".dropdown1");
+let dropdownItems1=document.querySelector(".dropdownItem1");
+
+dropdown1.addEventListener("click",()=>{
+    dropdownItems1.classList.toggle("hidden");
 })
