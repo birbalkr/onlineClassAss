3d200882de2eee8256a79b5ab954ef42a31ac1f1
birbalkr <birbalkr1435@gmail.com>
2026-06-21 00:26:05 +0530

    add video changer

--- slackWebPage/script.js
+++ slackWebPage/script.js
@@ -2,7 +2,17 @@ let dropdown = document.querySelector(".dropdown");
 let dropdownItems = document.querySelector(".dropdownItem");
 let dropdown1=document.querySelector(".dropdown1");
 let dropdownItems1=document.querySelector(".dropdownItem1");
+const video = document.getElementById("mainVideo");
 
+document.querySelectorAll(".video-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const fileName = btn.dataset.video;
+
+        video.src = `image/video/${fileName}`;
+        video.load();
+        video.play();
+    });
+});
 
 
 dropdown.addEventListener("click", () => {
