2c1d0f26c30e2a1514731342828cd26caee79820
birbalkr <birbalkr1435@gmail.com>
2026-06-20 22:43:49 +0530

    make home page

--- slackWebPage/script.js
+++ slackWebPage/script.js
@@ -1,13 +1,17 @@
 let dropdown = document.querySelector(".dropdown");
 let dropdownItems = document.querySelector(".dropdownItem");
-
-dropdown.addEventListener("click", () => {
-    dropdownItems.classList.toggle("hidden");
-})
-
 let dropdown1=document.querySelector(".dropdown1");
 let dropdownItems1=document.querySelector(".dropdownItem1");
 
+
+
+dropdown.addEventListener("click", () => {
+    dropdownItems.classList.toggle("hidden");
+    dropdownItems1.classList.add("hidden");
+})
+
 dropdown1.addEventListener("click",()=>{
     dropdownItems1.classList.toggle("hidden");
+    dropdownItems.classList.add("hidden");
 })
+
