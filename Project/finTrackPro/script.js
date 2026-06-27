5c29c0ffa2515db920f0f39561ed611deb672525
birbalkr <birbalkr1435@gmail.com>
2026-06-28 02:52:14 +0530

    side bar and card

---
+++ Project/finTrackPro/script.js
@@ -0,0 +1,21 @@
+// chartjs 
+const ctx = document.getElementById('myChart');
+let income = 55;
+let expense = 30;
+new Chart(ctx, { 
+    type: 'bar',
+    data: {
+        labels: ['Income Vs Expense'],
+        datasets: [{
+            label: 'Income',
+            data: [`${income}`],
+            borderWidth: 1
+        },
+    {
+            label: 'Expense',
+            data: [`${expense}`],
+            borderWidth: 1
+        }]
+    },
+    
+});
