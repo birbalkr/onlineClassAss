094817058c9e3fe3f63d879e3958594003c2f21b
birbalkr <birbalkr1435@gmail.com>
2026-06-28 22:17:30 +0530

    Add register

--- Project/finTrackPro/script.js
+++ Project/finTrackPro/script.js
@@ -1,4 +1,4 @@
-// login code 
+
 if (window.location.pathname.includes("index.html")) {
 
     if (localStorage.getItem("login") !== "true") {
@@ -8,7 +8,42 @@ if (window.location.pathname.includes("index.html")) {
     }
 
 }
-// register code 
+// register code
+let regUsername = document.querySelector("#regUsrename");
+let regPasswd = document.querySelector("#regPasswd");
+let regBtn = document.querySelector("#regBtn");
+let passwdEye = document.querySelector("#passwdEye");
+
+passwdEye.addEventListener("click", function () {
+    if (regPasswd.type === "password") {
+        regPasswd.type = "text";
+        passwdEye.classList.remove("fa-eye-slash");
+        passwdEye.classList.add("fa-eye");
+    } else {
+        regPasswd.type = "password";
+        passwdEye.classList.remove("fa-eye");
+        passwdEye.classList.add("fa-eye-slash");
+    }
+});
+
+regBtn.addEventListener("click", function () {
+    regUsername = regUsername.value;
+    regPasswd = regPasswd.value;
+
+    if (regUsername === "" || regPasswd === "") {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    localStorage.setItem("username", regUsername);
+    localStorage.setItem("password", regPasswd);
+    localStorage.setItem("login", "true");
+
+    window.location.href = "index.html";
+});
+
+// login code 
+
 // leftSize code 
 
 // Navbar code 
