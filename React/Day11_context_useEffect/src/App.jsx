4f29a3b13f447b588edfd59b545ae7ecb8e80aa6
birbalkr <birbalkr1435@gmail.com>
2026-07-15 07:08:10 +0530

    learn useEffect

--- React/Day11_context_useEffect/src/App.jsx
+++ React/Day11_context_useEffect/src/App.jsx
@@ -2,16 +2,26 @@
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
-import { ContextProvider, MyStore } from './context/MyContext';
+import { useEffect, useState } from 'react';
 
 function App() {
   console.log("app rendering......");
+  useEffect(() => {
+    console.log("UseEffect rendering.......");
+  }, [])
 
+  // [] run only once when the component is mounted
+  // 'count' is a state variable, setCount is a function to update the state variable
+
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <ContextProvider>
+      {/* <ContextProvider>
         <Home />
-      </ContextProvider>
+      </ContextProvider> */}
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <Home />
       <About />
       <Contact />
     </div>
--- React/Day11_context_useEffect/src/components/Home.jsx
+++ React/Day11_context_useEffect/src/components/Home.jsx
@@ -1,14 +1,10 @@
-import React, { useContext } from 'react'
+
 import { MyStore } from '../context/MyContext';
 
 function Home() {
-    let { count, setCount } = useContext(MyStore);
-    console.log("home rendering.......");
 
     return (
-        <>
-            <div>Home {count}</div>
-            <button onClick={() => setCount(count + 1)}>+1</button></>
+        <div>Home </div>
     )
 }
 
