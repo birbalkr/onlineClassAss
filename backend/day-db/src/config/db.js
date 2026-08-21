0087b920a2964abad10c31aec1526e02ae33b020
birbalkr <birbalkr1435@gmail.com>
2026-08-21 22:47:43 +0530

    add env file

--- backend/day-db/src/config/db.js
+++ backend/day-db/src/config/db.js
@@ -1,13 +1,23 @@
 const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
 
 const connectdb = async () => {
   try {
-    await mongoose.connect("mongodb+srv://birbal252397_db_user:1sLssSR4MVtbNi5g@cluster0.qqjrnkp.mongodb.net/");
+    await mongoose.connect(
+      `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.qqjrnkp.mongodb.net/`,
+    );
+
     console.log("DB connected successfully");
   } catch (error) {
     console.log("Error in DB.......", error);
+
+    console.log("***************************");
+
+    console.log(USERNAME);
   }
 };
-
 
 module.exports = connectdb;
